050bc93018091a82c8f8f092e279d6aa84f80615
prit_08 <pritjasani007@gmail.com>
2025-12-29 18:21:46 +0530

    final changes to deploy

--- config/database.js
+++ config/database.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 
 // Create MySQL connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'codevimarsh',
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_NAME,
+port: process.env.DB_PORT,
+
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
